98bd4f1c244f31b13e4afff644770c3dd0a41daf
Kauan José <kauanjose1255@gmail.com>
2024-03-22 10:33:16 -0300

    Ajuste serializers

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -3,15 +3,11 @@ from rest_framework import serializers, response
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'first_name', 'password']
         extra_kwargs = {
-            'password': {'write_only': True},
+            'first_name': {'required': True},
+            'email': {'required': True}
         }
-
-    def create(self, validated_data):
-        email = validated_data.get('email', None)
-        if email is None:
-            raise serializers.ValidationError("O campo de email é obrigatório")
-        return super().create(validated_data)
