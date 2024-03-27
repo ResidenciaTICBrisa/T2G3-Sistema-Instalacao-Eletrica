f07c037287edef4e03fd60aac8c3060e7fe587f1
Pedro Lucas Garcia <190115548@aluno.unb.br>
2024-03-27 10:18:53 -0300

    Adiciona novos campos ao Serializer de User

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -6,9 +6,12 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'password']
+        fields = ['id', 'password', 'username', 'first_name', 'email', 'is_active', 'date_joined', 'groups']
         extra_kwargs = {
             'password': {'write_only': True},
+            'first_name': {'required': True},
             'email': {'required': True},
-            'first_name': {'required': True}
+            'is_active': {'read_only': True},
+            'date_joined': {'read_only': True},
+            'groups': {'read_only': True}
         }
