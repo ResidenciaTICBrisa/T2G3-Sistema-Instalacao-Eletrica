978a47d6a8c0a24e642d3ae098d6ecca5a34c821
OscarDeBrito <oscarcorreiadebrito@gmail.com>
2024-04-01 16:45:51 -0300

    Remove arquivos do pages

--- api/systems/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class System(models.Model):
-
-    name = models.CharField(max_length=50)
--- frontend/sige_ie/android/app/src/main/kotlin/com/example/sige_ie/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sige_ie
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
