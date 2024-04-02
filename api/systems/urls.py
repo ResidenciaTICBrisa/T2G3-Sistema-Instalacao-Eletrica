f0ccf5d5baff3fe83025922cfcd1cb5818aa300a
Kauan José <kauanjose1255@gmail.com>
2024-04-02 13:59:14 -0300

    ajustando2

---
+++ api/systems/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import SystemViewDetail, SystemViewList
+
+urlpatterns = [
+    path('systems/', SystemViewList.as_view()),
+    path('systems/<pk>/', SystemViewDetail.as_view())
+]
+
