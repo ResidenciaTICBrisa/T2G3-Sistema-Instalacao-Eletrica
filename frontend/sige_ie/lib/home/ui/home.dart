9f78ef2a8a168dd949bfa85e7962d46a18981afa
EngDann <danilodemeloribeiro0@gmail.com>
2024-04-18 17:33:09 -0300

    FrontEnd: Base da home

--- frontend/sige_ie/lib/home/ui/home.dart
+++ frontend/sige_ie/lib/home/ui/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-// Importe suas páginas personalizadas
 import '../../users/feature/profile.dart';
 import '../../screens/facilities.dart';
 import '../../maps/feature/maps.dart';
@@ -30,9 +28,6 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-      ),
       body: PageView(
         controller: _pageController,
         onPageChanged: (index) {
@@ -41,59 +36,90 @@ class _HomePageState extends State<HomePage> {
           });
         },
         children: <Widget>[
-          Text(''), // Assumindo que esta é sua HomePage temporária
+          buildHomePage(context),
           FacilitiesPage(),
           MapsPage(),
           ProfilePage(),
         ],
       ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          ),
-          boxShadow: [
-            BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
-          ],
+      bottomNavigationBar: buildBottomNavigationBar(),
+    );
+  }
+
+  Widget buildHomePage(BuildContext context) {
+    return Column(
+      children: [
+        AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Color(0xff123c75),
+          elevation: 0,
         ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          ),
-          child: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home, size: 35),
-                  label: 'Home',
-                  backgroundColor: Color(0xFFF1F60E)),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.build, size: 35),
-                  label: 'Instalações',
-                  backgroundColor: Color(0xFFF1F60E)),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.map, size: 35),
-                  label: 'Mapa',
-                  backgroundColor: Color(0xFFF1F60E)),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.person, size: 35),
-                  label: 'Perfil',
-                  backgroundColor: Color(0xFFF1F60E)),
-            ],
-            currentIndex: _selectedIndex,
-            selectedItemColor: Color(0xFF123C75),
-            unselectedItemColor: const Color.fromARGB(255, 145, 142, 142),
-            onTap: _onItemTapped,
-            selectedLabelStyle: TextStyle(
-              color: Color(0xFF123C75),
-              fontSize: 14,
-              fontWeight: FontWeight.bold,
-            ),
-            unselectedLabelStyle: TextStyle(
-              fontSize: 12,
-            ),
-          ),
+        Container(
+          color: Color(0xff123c75),
+          height: MediaQuery.of(context).size.height * 0.30,
+        ),
+        Spacer(),
+        buildSmallRectangle(context),
+        buildSmallRectangle(context),
+        Spacer(),
+      ],
+    );
+  }
+
+  Widget buildSmallRectangle(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xff123c75),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+        ],
+      ),
+      height: 100,
+      width: MediaQuery.of(context).size.width * 0.8,
+      margin: EdgeInsets.symmetric(vertical: 10),
+    );
+  }
+
+  Widget buildBottomNavigationBar() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        boxShadow: [
+          BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home, size: 35),
+                label: 'Home',
+                backgroundColor: Color(0xFFF1F60E)),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.build, size: 35),
+                label: 'Instalações',
+                backgroundColor: Color(0xFFF1F60E)),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.map, size: 35),
+                label: 'Mapa',
+                backgroundColor: Color(0xFFF1F60E)),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person, size: 35),
+                label: 'Perfil',
+                backgroundColor: Color(0xFFF1F60E)),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Color(0xFF123C75),
+          unselectedItemColor: const Color.fromARGB(255, 145, 142, 142),
+          onTap: _onItemTapped,
         ),
       ),
     );
