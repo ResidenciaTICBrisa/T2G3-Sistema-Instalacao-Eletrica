797d006d2358d9d573f59a4fbb5231aedf63f9c3
Kauan José <kauanjose1255@gmail.com>
2024-04-18 15:43:25 -0300

    Adiciona euquipments create
    Co-authored-by: OscarDeBrito <oscarcorreiadebrito@gmail.com>

--- api/systems/models.py
+++ api/systems/models.py
@@ -7,3 +7,11 @@ class System(models.Model):
 
     def __str__(self):
         return self.name
+    
+class AtmosphericDischargeSystem(models.Model):
+    place = models.ForeignKey(Place, related_name='atmospheric_discharge_systems', on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, related_name='atmospheric_discharge_systems', on_delete=models.CASCADE)
+    system = models.ForeignKey('systems.System', related_name='atmospheric_discharge_systems', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'atmospheric_discharge_systems'
