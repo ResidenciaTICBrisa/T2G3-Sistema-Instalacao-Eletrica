6beac563bc462a02b732cfad4eda5eb19f1e8d06
ramires <ramiresrch15@gmail.com>
2024-04-29 13:45:57 -0300

    adicionar andar da sala e nome do sala

--- frontend/sige_ie/lib/places/register/room_state.dart
+++ frontend/sige_ie/lib/places/register/room_state.dart
@@ -9,6 +9,9 @@ class Roomlocation extends StatefulWidget {
 
 class _RoomlocationState extends State<Roomlocation>{
 
+  String? selectedFloor;
+  final List<String> floors = ['Andar 1','Andar 2','Andar 3','Andar 4','Andar 5'];
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -39,6 +42,72 @@ class _RoomlocationState extends State<Roomlocation>{
                   color:Colors.white )),
               ),
             ),
+            SizedBox(height:60),
+             Padding(
+              padding:const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:<Widget>[
+                Text('Andar',
+                style: TextStyle(
+                  fontSize:16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black )),
+                  SizedBox(height:10),
+                  Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: 
+                     BorderRadius.circular(10),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: DropdownButton<String>(
+                          value: selectedFloor,
+                          hint: Text('Selecione o Andar'),
+                          isExpanded: true,
+                          items: floors.map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (newValue) {
+                            setState(() {
+                              selectedFloor = newValue;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                SizedBox(height:40),
+                Text('Sala',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color:Colors.black)),
+                    SizedBox(height: 10),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: 
+                         BorderRadius.circular(10),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Digite o nome da Sala',
+                          border:InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 10),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 60),
+                    
+              ],
+              ), )
           ],
         ),
       ),
