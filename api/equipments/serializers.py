7297049d9507b50371dc4295eabe363cfc819bf6
Kauan José <kauanjose1255@gmail.com>
2024-04-29 14:58:36 -0300

    Backend: cria serializers de todos sistemas

--- api/equipments/serializers.py
+++ api/equipments/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import EquipmentType, EquipmentDetail, AtmosphericDischargeEquipment, FireAlarmEquipment, SructeredCablingEquipment
+from .models import *
 
 class EquipmentTypeSerializer(serializers.ModelSerializer):
 
@@ -31,3 +31,34 @@ class SructeredCablingEquipmentSerializer(serializers.ModelSerializer):
         model = SructeredCablingEquipment
         fields = '__all__'    
 
+class DistributionBoardEquipmentSerializer(serializers.ModelSerializer):
+
+     class Meta:
+        model = DistributionBoardEquipment
+        fields = '__all__'    
+   
+class ElectricalCircuitEquipmentSerializer(serializers.ModelSerializer):
+
+     class Meta:
+        model = ElectricalCircuitEquipment
+        fields = '__all__'    
+   
+
+class ElectricalLineEquipmentSerializer(serializers.ModelSerializer):
+
+     class Meta:
+        model = ElectricalLineEquipment
+        fields = '__all__'    
+   
+class ElectricalLoadEquipmentSerializer(serializers.ModelSerializer):
+
+     class Meta:
+        model = ElectricalLoadEquipment
+        fields = '__all__'    
+    
+class IluminationEquipmentSerializer(serializers.ModelSerializer):
+
+     class Meta:
+        model = IluminationEquipment
+        fields = '__all__'    
+   
