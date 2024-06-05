5553b65c65a3b62a79be10cece43c9fe4b8aeb59
Kauan José <kauanjose1255@gmail.com>
2024-06-05 14:36:22 -0300

    Backend: ajuste models equipments

--- api/equipments/models.py
+++ api/equipments/models.py
@@ -37,8 +37,9 @@ class EquipmentDetail(models.Model):
 class EquipmentPhoto(models.Model):
 
     photo = models.ImageField(null=True, upload_to='equipment_photos/')
-    description = models.CharField(max_length=50)
-    equipment_detail = models.ForeignKey(EquipmentDetail, on_delete=models.CASCADE, null=True)
+    description = models.CharField(max_length=50, null=True)
+    equipmentType = models.ForeignKey(EquipmentType, on_delete=models.CASCADE, null=True)
+    personalEquipmentType = models.ForeignKey(PersonalEquipmentType, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.description
