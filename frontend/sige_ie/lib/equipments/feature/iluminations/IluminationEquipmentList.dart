5c7a86cba4fb62abb71db90bcd49acdfd8515c1e
EngDann <danilodemeloribeiro0@gmail.com>
2024-06-10 16:19:40 -0300

    Refatorando iluminação

--- frontend/sige_ie/lib/equipments/feature/iluminations/IluminationEquipmentList.dart
+++ frontend/sige_ie/lib/equipments/feature/iluminations/IluminationEquipmentList.dart
@@ -72,6 +72,7 @@ class listIluminationEquipment extends StatelessWidget {
               ),
               child: Center(
                 child: Text('$areaName - $systemTitle',
+                    textAlign: TextAlign.center,
                     style: const TextStyle(
                         fontSize: 26,
                         fontWeight: FontWeight.bold,
