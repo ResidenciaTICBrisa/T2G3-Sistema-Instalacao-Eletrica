5d1f6c17b09a2612f8dbafed05a9aaa02dde3ed4
OscarDeBrito <oscarcorreiadebrito@gmail.com>
2024-06-24 16:01:43 -0300

    Atualiza a página de listagem de equipamentos linha eletrica
    Co-authored-by: Danilo <danilodemeloribeiro0@gmail.com>

--- frontend/sige_ie/lib/equipments/data/eletrical_line/eletrical_line_service.dart
+++ frontend/sige_ie/lib/equipments/data/eletrical_line/eletrical_line_service.dart
@@ -37,12 +37,22 @@ class EletricalLineEquipmentService {
       final response = await client.get(Uri.parse(url));
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body);
-        return data.map((item) => item['name'] as String).toList();
+        _logger.info('API response data: $data');
+        return data.map((item) {
+          if (item['equipment'].containsKey('generic_equipment_category')) {
+            return item['equipment']['generic_equipment_category'] as String;
+          } else if (item['equipment']
+              .containsKey('personal_equipment_category')) {
+            return item['equipment']['personal_equipment_category'] as String;
+          } else {
+            return 'Unknown Equipment';
+          }
+        }).toList();
       } else {
-        throw Exception('Failed to load structured cabling equipment');
+        throw Exception('Failed to load electrical-lines equipment');
       }
     } catch (e) {
-      _logger.info('Error during get structured cabling equipment list: $e');
+      _logger.info('Error during get electrical-lines equipment list: $e');
       return [];
     }
   }
