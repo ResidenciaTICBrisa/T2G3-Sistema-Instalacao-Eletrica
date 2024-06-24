f51317246fa26a7304789c9128f2ef430bc99c01
OscarDeBrito <oscarcorreiadebrito@gmail.com>
2024-06-24 15:58:42 -0300

    Atualiza a página de listagem de equipamentos atmosféricos
    Co-authored-by: Danilo <danilodemeloribeiro0@gmail.com>

--- frontend/sige_ie/lib/equipments/feature/atmospheric-discharges/atmospheric_discharges_list.dart
+++ frontend/sige_ie/lib/equipments/feature/atmospheric-discharges/atmospheric_discharges_list.dart
@@ -70,6 +70,14 @@ class _ListAtmosphericEquipmentState extends State<ListAtmosphericEquipment> {
     );
   }
 
+  void _editEquipment(BuildContext context, String equipment) {
+    // Implement the logic to edit the equipment
+  }
+
+  void _deleteEquipment(BuildContext context, String equipment) {
+    // Implement the logic to delete the equipment
+  }
+
   @override
   Widget build(BuildContext context) {
     String systemTitle = 'DESCARGAS ATMOSFÉRICAS';
@@ -129,8 +137,34 @@ class _ListAtmosphericEquipmentState extends State<ListAtmosphericEquipment> {
                       : equipmentList.isNotEmpty
                           ? Column(
                               children: equipmentList.map((equipment) {
-                                return ListTile(
-                                  title: Text(equipment),
+                                return Container(
+                                  color: AppColors.sigeIeBlue,
+                                  margin:
+                                      const EdgeInsets.symmetric(vertical: 5),
+                                  child: ListTile(
+                                    title: Text(
+                                      equipment,
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                    ),
+                                    trailing: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        IconButton(
+                                          icon: const Icon(Icons.edit,
+                                              color: Colors.blue),
+                                          onPressed: () => _editEquipment(
+                                              context, equipment),
+                                        ),
+                                        IconButton(
+                                          icon: const Icon(Icons.delete,
+                                              color: Colors.red),
+                                          onPressed: () => _deleteEquipment(
+                                              context, equipment),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
                                 );
                               }).toList(),
                             )
