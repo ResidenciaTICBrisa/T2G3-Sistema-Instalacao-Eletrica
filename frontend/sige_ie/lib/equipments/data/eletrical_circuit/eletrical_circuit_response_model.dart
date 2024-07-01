b5ce8a5c44cd35bdf10d16a3ebaa28fed3911c92
Pedro Lucas Garcia <190115548@aluno.unb.br>
2024-07-01 12:35:16 -0300

    frontend: corrige model de response de circuitos elétricos

--- frontend/sige_ie/lib/equipments/data/eletrical_circuit/eletrical_circuit_response_model.dart
+++ frontend/sige_ie/lib/equipments/data/eletrical_circuit/eletrical_circuit_response_model.dart
@@ -1,11 +1,13 @@
 class EletricalCircuitEquipmentResponseModel {
   int id;
-  String area;
+  int area;
+  String equipmentCategory;
   int system;
 
   EletricalCircuitEquipmentResponseModel({
     required this.id,
     required this.area,
+    required this.equipmentCategory,
     required this.system,
   });
 
@@ -13,16 +15,9 @@ class EletricalCircuitEquipmentResponseModel {
       Map<String, dynamic> json) {
     return EletricalCircuitEquipmentResponseModel(
       id: json['id'],
-      area: json['name'],
+      area: json['area'],
+      equipmentCategory: json['equipment_category'],
       system: json['system'],
     );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'area': area,
-      'system': system,
-    };
   }
 }
