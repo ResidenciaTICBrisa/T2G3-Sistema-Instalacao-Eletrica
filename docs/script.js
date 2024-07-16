2718d87fa77416d1abe97ecd35bd64a4aa94bd17
EngDann <danilodemeloribeiro0@gmail.com>
2024-07-16 14:28:27 -0300

    Página refatorada

--- docs/script.js
+++ docs/script.js
@@ -44,3 +44,32 @@ document.addEventListener("scroll", function () {
 document.getElementById("nav-toggle").addEventListener("click", function () {
     document.getElementById("nav-content").classList.toggle("hidden");
 });
+
+$(document).ready(function () {
+    $(".team-carousel").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+});
