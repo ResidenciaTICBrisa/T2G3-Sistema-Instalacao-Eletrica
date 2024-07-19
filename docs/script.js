8057b6df64bd0ec359a05c8868a9004a4d0ddc98
EngDann <danilodemeloribeiro0@gmail.com>
2024-07-19 12:21:22 -0300

    logo consertada

--- docs/script.js
+++ docs/script.js
@@ -5,41 +5,6 @@ var navaction = document.getElementById("navAction");
 var brandname = document.getElementById("brandname");
 var toToggle = document.querySelectorAll(".toggleColour");
 
-document.addEventListener("scroll", function () {
-    scrollpos = window.scrollY;
-
-    if (scrollpos > 10) {
-        header.classList.add("bg-white");
-        navaction.classList.remove("bg-white");
-        navaction.classList.add("gradient");
-        navaction.classList.remove("text-gray-800");
-        navaction.classList.add("text-white");
-        // Use to switch toggleColour colours
-        for (var i = 0; i < toToggle.length; i++) {
-            toToggle[i].classList.add("text-gray-800");
-            toToggle[i].classList.remove("text-white");
-        }
-        header.classList.add("shadow");
-        navcontent.classList.remove("bg-gray-100");
-        navcontent.classList.add("bg-white");
-    } else {
-        header.classList.remove("bg-white");
-        navaction.classList.remove("gradient");
-        navaction.classList.add("bg-white");
-        navaction.classList.remove("text-white");
-        navaction.classList.add("text-gray-800");
-
-        for (var i = 0; i < toToggle.length; i++) {
-            toToggle[i].classList.add("text-white");
-            toToggle[i].classList.remove("text-gray-800");
-        }
-
-        header.classList.remove("shadow");
-        navcontent.classList.remove("bg-white");
-        navcontent.classList.add("bg-gray-100");
-    }
-});
-
 document.getElementById("nav-toggle").addEventListener("click", function () {
     document.getElementById("nav-content").classList.toggle("hidden");
 });
@@ -71,6 +36,11 @@ $(document).ready(function () {
             },
         ],
     });
+});
+
+document.getElementById("nav-toggle").addEventListener("click", function () {
+    this.classList.toggle("open");
+    document.getElementById("nav-content").classList.toggle("hidden");
 });
 
 document.getElementById("modeToggle").addEventListener("click", function () {
