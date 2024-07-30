5cd3a9952ca8c9ea74246090f23f3b7497c4e6b2
EngDann <danilodemeloribeiro0@gmail.com>
2024-07-30 14:36:32 -0300

    Response model distribution, desfazendo erro

--- frontend/sige_ie/lib/equipments/data/distribution/distribution_response_model.dart
+++ frontend/sige_ie/lib/equipments/data/distribution/distribution_response_model.dart
@@ -4,22 +4,38 @@ class DistributionResponseModel {
   int equipment;
   int system;
   int quantity;
+  String? power;
+  bool dr;
+  bool dps;
+  bool grounding;
+  String? typeMaterial;
+  String? methodInstallation;
 
-  DistributionResponseModel({
-    required this.id,
-    required this.area,
-    required this.equipment,
-    required this.system,
-    required this.quantity,
-  });
+  DistributionResponseModel(
+      {required this.id,
+      required this.area,
+      required this.equipment,
+      required this.system,
+      required this.quantity,
+      required this.power,
+      required this.dr,
+      required this.dps,
+      required this.grounding,
+      required this.typeMaterial,
+      required this.methodInstallation});
 
   factory DistributionResponseModel.fromJson(Map<String, dynamic> json) {
     return DistributionResponseModel(
-      id: json['id'],
-      area: json['area'],
-      equipment: json['equipment'],
-      system: json['system'],
-      quantity: json['quantity'],
-    );
+        id: json['id'],
+        area: json['area'],
+        equipment: json['equipment'],
+        system: json['system'],
+        quantity: json['quantity'],
+        power: json['power'],
+        dr: json['dr'],
+        dps: json['dps'],
+        grounding: json['grounding'],
+        typeMaterial: json['typeMaterial'],
+        methodInstallation: json['methodInstallation']);
   }
 }
--- frontend/sige_ie/lib/equipments/data/structured_cabling/structured_cabling_response_model.dart
+++ frontend/sige_ie/lib/equipments/data/structured_cabling/structured_cabling_response_model.dart
@@ -4,38 +4,22 @@ class StructuredCablingResponseModel {
   int equipment;
   int system;
   int quantity;
-  String? power;
-  bool dr;
-  bool dps;
-  bool grounding;
-  String? typeMaterial;
-  String? methodInstallation;
 
-  StructuredCablingResponseModel(
-      {required this.id,
-      required this.area,
-      required this.equipment,
-      required this.system,
-      required this.quantity,
-      required this.power,
-      required this.dr,
-      required this.dps,
-      required this.grounding,
-      required this.typeMaterial,
-      required this.methodInstallation});
+  StructuredCablingResponseModel({
+    required this.id,
+    required this.area,
+    required this.equipment,
+    required this.system,
+    required this.quantity,
+  });
 
   factory StructuredCablingResponseModel.fromJson(Map<String, dynamic> json) {
     return StructuredCablingResponseModel(
-        id: json['id'],
-        area: json['area'],
-        equipment: json['equipment'],
-        system: json['system'],
-        quantity: json['quantity'],
-        power: json['power'],
-        dr: json['dr'],
-        dps: json['dps'],
-        grounding: json['grounding'],
-        typeMaterial: json['typeMaterial'],
-        methodInstallation: json['methodInstallation']);
+      id: json['id'],
+      area: json['area'],
+      equipment: json['equipment'],
+      system: json['system'],
+      quantity: json['quantity'],
+    );
   }
 }
