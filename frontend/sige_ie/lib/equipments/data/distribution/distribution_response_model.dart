b7670feb7a535f8f680ecae9c6e5dac6f42a84ee
EngDann <danilodemeloribeiro0@gmail.com>
2024-07-30 14:33:26 -0300

    Response model distribution

--- frontend/sige_ie/lib/equipments/data/distribution/distribution_response_model.dart
+++ frontend/sige_ie/lib/equipments/data/distribution/distribution_response_model.dart
@@ -1,23 +1,25 @@
-class DistributionEquipmentResponseModel {
+class DistributionResponseModel {
   int id;
   int area;
-  String equipmentCategory;
+  int equipment;
   int system;
+  int quantity;
 
-  DistributionEquipmentResponseModel({
+  DistributionResponseModel({
     required this.id,
     required this.area,
-    required this.equipmentCategory,
+    required this.equipment,
     required this.system,
+    required this.quantity,
   });
 
-  factory DistributionEquipmentResponseModel.fromJson(
-      Map<String, dynamic> json) {
-    return DistributionEquipmentResponseModel(
+  factory DistributionResponseModel.fromJson(Map<String, dynamic> json) {
+    return DistributionResponseModel(
       id: json['id'],
       area: json['area'],
-      equipmentCategory: json['equipment_category'],
+      equipment: json['equipment'],
       system: json['system'],
+      quantity: json['quantity'],
     );
   }
 }
