4071bbbe878e85f62c7029f175bf15cf28666f64
EngDann <danilodemeloribeiro0@gmail.com>
2024-07-30 14:09:36 -0300

    Service ilumination

--- frontend/sige_ie/lib/equipments/data/iluminations/ilumination_service.dart
+++ frontend/sige_ie/lib/equipments/data/iluminations/ilumination_service.dart
@@ -4,6 +4,9 @@ import 'package:http_interceptor/http_interceptor.dart';
 import 'package:logging/logging.dart';
 import 'package:sige_ie/core/data/auth_interceptor.dart';
 import 'package:sige_ie/core/data/universalURL.dart';
+import 'package:sige_ie/equipments/data/iluminations/ilumination_request_model.dart';
+import 'package:sige_ie/equipments/data/iluminations/ilumination_response.dart';
+import 'package:sige_ie/equipments/data/iluminations/ilumination_response_by_area_model.dart';
 import 'package:sige_ie/main.dart';
 
 class IluminationEquipmentService {
@@ -13,29 +16,90 @@ class IluminationEquipmentService {
     interceptors: [AuthInterceptor(cookieJar)],
   );
 
-  Future<List<String>> getIluminationListByArea(int areaId) async {
-    final url = '${baseUrl}iluminations/by-area/$areaId';
+  Future<List<IluminationEquipmentResponseByAreaModel>>
+      getIluminationListByArea(int areaId) async {
+    var url = Uri.parse('${baseUrl}iluminations/by-area/$areaId/');
     try {
-      final response = await client.get(Uri.parse(url));
+      var response = await client.get(url);
+
       if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
-        _logger.info('API response data: $data');
-        return data.map((item) {
-          if (item['equipment'].containsKey('generic_equipment_category')) {
-            return item['equipment']['generic_equipment_category'] as String;
-          } else if (item['equipment']
-              .containsKey('personal_equipment_category')) {
-            return item['equipment']['personal_equipment_category'] as String;
-          } else {
-            return 'Unknown Equipment';
-          }
-        }).toList();
+        List<dynamic> dataList = jsonDecode(response.body);
+        return dataList
+            .map((data) =>
+                IluminationEquipmentResponseByAreaModel.fromJson(data))
+            .toList();
       } else {
+        _logger.info(
+            'Failed to load ilumination equipment with status code: ${response.statusCode}');
+        _logger.info('Response body: ${response.body}');
         throw Exception('Failed to load ilumination equipment');
       }
     } catch (e) {
       _logger.info('Error during get ilumination equipment list: $e');
-      return [];
+      throw Exception('Failed to load ilumination equipment');
+    }
+  }
+
+  Future<void> deleteIlumination(int IluminationId) async {
+    var url = Uri.parse('${baseUrl}iluminations/$IluminationId/');
+    try {
+      var response = await client.delete(url);
+      if (response.statusCode == 204) {
+        _logger.info(
+            'Successfully deleted ilumination equipment with ID: $IluminationId');
+      } else {
+        _logger.info(
+            'Failed to delete ilumination equipment with status code: ${response.statusCode}');
+        _logger.info('Response body: ${response.body}');
+        throw Exception('Failed to delete ilumination equipment');
+      }
+    } catch (e) {
+      _logger.info('Error during delete ilumination equipment: $e');
+      throw Exception('Failed to delete ilumination equipment');
+    }
+  }
+
+  Future<IluminationResponseModel> getIluminationById(int IluminationId) async {
+    var url = Uri.parse('${baseUrl}iluminations/$IluminationId/');
+    try {
+      var response = await client.get(url);
+      if (response.statusCode == 200) {
+        var jsonResponse = jsonDecode(response.body);
+        return IluminationResponseModel.fromJson(jsonResponse);
+      } else {
+        _logger.info(
+            'Failed to load ilumination with status code: ${response.statusCode}');
+        throw Exception('Failed to load ilumination');
+      }
+    } catch (e) {
+      _logger.info('Error during get ilumination: $e');
+      throw Exception('Failed to load ilumination');
+    }
+  }
+
+  Future<bool> updateIlumination(int IluminationId,
+      IluminationRequestModel IluminationIdRequestModel) async {
+    var url = Uri.parse('${baseUrl}iluminations/$IluminationId/');
+
+    try {
+      var response = await client.put(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(IluminationIdRequestModel.toJson()),
+      );
+
+      if (response.statusCode == 200) {
+        _logger.info(
+            'Successfully updated ilumination equipment with ID: $IluminationId');
+        return true;
+      } else {
+        _logger.info(
+            'Failed to update ilumination equipment with status code: ${response.statusCode}');
+        return false;
+      }
+    } catch (e) {
+      _logger.info('Error during update ilumination equipment: $e');
+      return false;
     }
   }
 }
