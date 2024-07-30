649f010b1bde9cb2451a805d8554b79fdaff1253
EngDann <danilodemeloribeiro0@gmail.com>
2024-07-30 14:50:03 -0300

    Service refrigerations

--- frontend/sige_ie/lib/equipments/data/refrigerations/refrigerations_service.dart
+++ frontend/sige_ie/lib/equipments/data/refrigerations/refrigerations_service.dart
@@ -4,6 +4,9 @@ import 'package:http_interceptor/http_interceptor.dart';
 import 'package:logging/logging.dart';
 import 'package:sige_ie/core/data/auth_interceptor.dart';
 import 'package:sige_ie/core/data/universalURL.dart';
+import 'package:sige_ie/equipments/data/refrigerations/refrigerations_request_model.dart';
+import 'package:sige_ie/equipments/data/refrigerations/refrigerations_response_by_area_model.dart';
+import 'package:sige_ie/equipments/data/refrigerations/refrigerations_response_model.dart';
 import 'package:sige_ie/main.dart';
 
 class RefrigerationsEquipmentService {
@@ -13,29 +16,91 @@ class RefrigerationsEquipmentService {
     interceptors: [AuthInterceptor(cookieJar)],
   );
 
-  Future<List<String>> getRefrigerationsListByArea(int areaId) async {
-    final url = '${baseUrl}refrigeration/by-area/$areaId';
+  Future<List<RefrigerationsEquipmentResponseByAreaModel>>
+      getRefrigerationsListByArea(int areaId) async {
+    var url = Uri.parse('${baseUrl}refrigerations/by-area/$areaId/');
     try {
-      final response = await client.get(Uri.parse(url));
+      var response = await client.get(url);
+
       if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
-        _logger.info('API response data: $data');
-        return data.map((item) {
-          if (item['equipment'].containsKey('generic_equipment_category')) {
-            return item['equipment']['generic_equipment_category'] as String;
-          } else if (item['equipment']
-              .containsKey('personal_equipment_category')) {
-            return item['equipment']['personal_equipment_category'] as String;
-          } else {
-            return 'Unknown Equipment';
-          }
-        }).toList();
+        List<dynamic> dataList = jsonDecode(response.body);
+        return dataList
+            .map((data) =>
+                RefrigerationsEquipmentResponseByAreaModel.fromJson(data))
+            .toList();
       } else {
-        throw Exception('Failed to load ilumination equipment');
+        _logger.info(
+            'Failed to load refrigerations equipment with status code: ${response.statusCode}');
+        _logger.info('Response body: ${response.body}');
+        throw Exception('Failed to load refrigerations equipment');
       }
     } catch (e) {
-      _logger.info('Error during get ilumination equipment list: $e');
-      return [];
+      _logger.info('Error during get refrigerations equipment list: $e');
+      throw Exception('Failed to load refrigerations equipment');
+    }
+  }
+
+  Future<void> deleteRefrigerations(int RefrigerationsId) async {
+    var url = Uri.parse('${baseUrl}refrigerations/$RefrigerationsId/');
+    try {
+      var response = await client.delete(url);
+      if (response.statusCode == 204) {
+        _logger.info(
+            'Successfully deleted refrigerations equipment with ID: $RefrigerationsId');
+      } else {
+        _logger.info(
+            'Failed to delete refrigerations equipment with status code: ${response.statusCode}');
+        _logger.info('Response body: ${response.body}');
+        throw Exception('Failed to delete refrigerations equipment');
+      }
+    } catch (e) {
+      _logger.info('Error during delete refrigerations equipment: $e');
+      throw Exception('Failed to delete refrigerations equipment');
+    }
+  }
+
+  Future<RefrigerationsResponseModel> getRefrigerationsById(
+      int RefrigerationsId) async {
+    var url = Uri.parse('${baseUrl}refrigerations/$RefrigerationsId/');
+    try {
+      var response = await client.get(url);
+      if (response.statusCode == 200) {
+        var jsonResponse = jsonDecode(response.body);
+        return RefrigerationsResponseModel.fromJson(jsonResponse);
+      } else {
+        _logger.info(
+            'Failed to load refrigerations with status code: ${response.statusCode}');
+        throw Exception('Failed to load refrigerations');
+      }
+    } catch (e) {
+      _logger.info('Error during get refrigerations: $e');
+      throw Exception('Failed to load refrigerations');
+    }
+  }
+
+  Future<bool> updateRefrigerations(int RefrigerationsId,
+      RefrigerationsRequestModel RefrigerationsIdRequestModel) async {
+    var url = Uri.parse('${baseUrl}refrigerations/$RefrigerationsId/');
+
+    try {
+      var response = await client.put(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(RefrigerationsIdRequestModel.toJson()),
+      );
+
+      if (response.statusCode == 200) {
+        _logger.info(
+            'Successfully updated refrigerations equipment with ID: $RefrigerationsId');
+        return true;
+      } else {
+        _logger.info(
+            'Failed to update refrigerations equipment with status code: ${response.statusCode}');
+        return false;
+      }
+    } catch (e) {
+      _logger.info('Error during update refrigerations equipment: $e');
+      return false;
     }
   }
 }
