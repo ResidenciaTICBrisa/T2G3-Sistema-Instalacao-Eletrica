78dc3de0f0a162b9718f9c4075fb581141b8f7c8
EngDann <danilodemeloribeiro0@gmail.com>
2024-07-30 13:57:43 -0300

    Request model ilumination

--- frontend/sige_ie/lib/equipments/data/iluminations/ilumination_request_model.dart
+++ frontend/sige_ie/lib/equipments/data/iluminations/ilumination_request_model.dart
@@ -1,16 +1,27 @@
 class IluminationRequestModel {
   int? area;
   int? system;
+  int? quantity;
+  int? power;
+  String tecnology;
+  String format;
 
-  IluminationRequestModel({
-    required this.area,
-    required this.system,
-  });
+  IluminationRequestModel(
+      {required this.area,
+      required this.system,
+      required this.quantity,
+      required this.format,
+      required this.power,
+      required this.tecnology});
 
   Map<String, dynamic> toJson() {
     return {
       'area': area,
       'system': system,
+      'quantity': quantity,
+      'format': format,
+      'power': power,
+      'tecnology': tecnology
     };
   }
 }
