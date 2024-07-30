ba731f983f02ce28f6203f6bc0d6c9e11e582160
EngDann <danilodemeloribeiro0@gmail.com>
2024-07-30 14:02:17 -0300

    Response area model ilumination

--- frontend/sige_ie/lib/equipments/data/iluminations/ilumination_response_by_area_model.dart
+++ frontend/sige_ie/lib/equipments/data/iluminations/ilumination_response_by_area_model.dart
@@ -4,23 +4,30 @@ class IluminationEquipmentResponseByAreaModel {
   String equipmentCategory;
   int system;
   int quantity;
+  int? power;
+  String tecnology;
+  String format;
 
-  IluminationEquipmentResponseByAreaModel({
-    required this.id,
-    required this.area,
-    required this.equipmentCategory,
-    required this.system,
-    required this.quantity,
-  });
+  IluminationEquipmentResponseByAreaModel(
+      {required this.id,
+      required this.area,
+      required this.equipmentCategory,
+      required this.system,
+      required this.quantity,
+      required this.format,
+      required this.power,
+      required this.tecnology});
 
   factory IluminationEquipmentResponseByAreaModel.fromJson(
       Map<String, dynamic> json) {
     return IluminationEquipmentResponseByAreaModel(
-      id: json['id'],
-      area: json['area'],
-      equipmentCategory: json['equipment_category'],
-      system: json['system'],
-      quantity: json['quantity'],
-    );
+        id: json['id'],
+        area: json['area'],
+        equipmentCategory: json['equipment_category'],
+        system: json['system'],
+        quantity: json['quantity'],
+        format: json['format'],
+        power: json['power'],
+        tecnology: json['tecnology']);
   }
 }
