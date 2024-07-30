29fead0d2226cd581665edebb5b2934d21722176
EngDann <danilodemeloribeiro0@gmail.com>
2024-07-30 13:36:57 -0300

    Service eletrical circuit

--- frontend/sige_ie/lib/equipments/data/eletrical_circuit/eletrical_circuit_service.dart
+++ frontend/sige_ie/lib/equipments/data/eletrical_circuit/eletrical_circuit_service.dart
@@ -4,6 +4,9 @@ import 'package:http_interceptor/http_interceptor.dart';
 import 'package:logging/logging.dart';
 import 'package:sige_ie/core/data/auth_interceptor.dart';
 import 'package:sige_ie/core/data/universalURL.dart';
+import 'package:sige_ie/equipments/data/eletrical_circuit/eletrical_circuit_request_model.dart';
+import 'package:sige_ie/equipments/data/eletrical_circuit/eletrical_circuit_response_by_area_model.dart';
+import 'package:sige_ie/equipments/data/eletrical_circuit/eletrical_circuit_response_model.dart';
 import 'package:sige_ie/main.dart';
 
 class EletricalCircuitEquipmentService {
@@ -13,29 +16,91 @@ class EletricalCircuitEquipmentService {
     interceptors: [AuthInterceptor(cookieJar)],
   );
 
-  Future<List<String>> getEletricalCircuitListByArea(int areaId) async {
-    final url = '${baseUrl}electrical-circuits/by-area/$areaId';
+  Future<List<EletricalCircuitEquipmentResponseByAreaModel>>
+      getEletricalCircuitListByArea(int areaId) async {
+    var url = Uri.parse('${baseUrl}electrical-circuits/by-area/$areaId/');
     try {
-      final response = await client.get(Uri.parse(url));
+      var response = await client.get(url);
+
       if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
-        _logger.info('API response data: $data');
-        return data.map((item) {
-          if (item['equipment'].containsKey('generic_equipment_category')) {
-            return item['equipment']['generic_equipment_category'] as String;
-          } else if (item['equipment']
-              .containsKey('personal_equipment_category')) {
-            return item['equipment']['personal_equipment_category'] as String;
-          } else {
-            return 'Unknown Equipment';
-          }
-        }).toList();
+        List<dynamic> dataList = jsonDecode(response.body);
+        return dataList
+            .map((data) =>
+                EletricalCircuitEquipmentResponseByAreaModel.fromJson(data))
+            .toList();
       } else {
-        throw Exception('Failed to load electrical-circuits equipment');
+        _logger.info(
+            'Failed to load eletrical circuit equipment with status code: ${response.statusCode}');
+        _logger.info('Response body: ${response.body}');
+        throw Exception('Failed to load eletrical circuit equipment');
       }
     } catch (e) {
-      _logger.info('Error during get electrical-circuits equipment list: $e');
-      return [];
+      _logger.info('Error during get eletrical circuit equipment list: $e');
+      throw Exception('Failed to load eletrical circuit equipment');
+    }
+  }
+
+  Future<void> deleteEletricalCircuit(int EletricalCircuitId) async {
+    var url = Uri.parse('${baseUrl}electrical-circuits/$EletricalCircuitId/');
+    try {
+      var response = await client.delete(url);
+      if (response.statusCode == 204) {
+        _logger.info(
+            'Successfully deleted eletrical circuit equipment with ID: $EletricalCircuitId');
+      } else {
+        _logger.info(
+            'Failed to delete eletrical circuit equipment with status code: ${response.statusCode}');
+        _logger.info('Response body: ${response.body}');
+        throw Exception('Failed to delete eletrical circuit equipment');
+      }
+    } catch (e) {
+      _logger.info('Error during delete eletrical circuit equipment: $e');
+      throw Exception('Failed to delete eletrical circuit equipment');
+    }
+  }
+
+  Future<EletricalCircuitResponseModel> getEletricalCircuitById(
+      int EletricalCircuitId) async {
+    var url = Uri.parse('${baseUrl}electrical-circuits/$EletricalCircuitId/');
+    try {
+      var response = await client.get(url);
+      if (response.statusCode == 200) {
+        var jsonResponse = jsonDecode(response.body);
+        return EletricalCircuitResponseModel.fromJson(jsonResponse);
+      } else {
+        _logger.info(
+            'Failed to load eletrical circuit with status code: ${response.statusCode}');
+        throw Exception('Failed to load eletrical circuit');
+      }
+    } catch (e) {
+      _logger.info('Error during get eletrical circuit: $e');
+      throw Exception('Failed to load eletrical circuit');
+    }
+  }
+
+  Future<bool> updateEletricalCircuit(int EletricalCircuitId,
+      EletricalCircuitRequestModel EletricalCircuitIdRequestModel) async {
+    var url = Uri.parse('${baseUrl}electrical-circuits/$EletricalCircuitId/');
+
+    try {
+      var response = await client.put(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(EletricalCircuitIdRequestModel.toJson()),
+      );
+
+      if (response.statusCode == 200) {
+        _logger.info(
+            'Successfully updated eletrical circuit equipment with ID: $EletricalCircuitId');
+        return true;
+      } else {
+        _logger.info(
+            'Failed to update eletrical circuit equipment with status code: ${response.statusCode}');
+        return false;
+      }
+    } catch (e) {
+      _logger.info('Error during update eletrical circuit equipment: $e');
+      return false;
     }
   }
 }
