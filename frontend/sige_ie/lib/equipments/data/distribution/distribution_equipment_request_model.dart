2089a48eb04d0977da1fb6f013b2f8998455a25a
EngDann <danilodemeloribeiro0@gmail.com>
2024-07-30 14:23:01 -0300

    Equipment request model distribution

--- frontend/sige_ie/lib/equipments/data/distribution/distribution_equipment_request_model.dart
+++ frontend/sige_ie/lib/equipments/data/distribution/distribution_equipment_request_model.dart
@@ -4,36 +4,18 @@ class DistributionEquipmentRequestModel {
   int? genericEquipmentCategory;
   int? personalEquipmentCategory;
   DistributionRequestModel? distributionRequestModel;
-  String? power;
-  bool dr;
-  bool dps;
-  bool grounding;
-  String? typeMaterial;
-  String? methodInstallation;
 
   DistributionEquipmentRequestModel({
     required this.genericEquipmentCategory,
     required this.personalEquipmentCategory,
     required this.distributionRequestModel,
-    this.power,
-    this.dr = false,
-    this.dps = false,
-    this.grounding = false,
-    this.typeMaterial,
-    this.methodInstallation,
   });
 
   Map<String, dynamic> toJson() {
     return {
       'generic_equipment_category': genericEquipmentCategory,
       'personal_equipment_category': personalEquipmentCategory,
-      'distribution_board_equipment': distributionRequestModel?.toJson(),
-      'power': power,
-      'dr': dr,
-      'dps': dps,
-      'grounding': grounding,
-      'type_material': typeMaterial,
-      'method_installation': methodInstallation,
+      'structured_cabling_equipment': distributionRequestModel?.toJson(),
     };
   }
 }
