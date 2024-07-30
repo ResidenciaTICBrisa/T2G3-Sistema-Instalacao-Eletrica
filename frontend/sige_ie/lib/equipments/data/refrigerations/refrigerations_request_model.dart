b350f6378717d377eb2e5481d96431a3f418a4de
EngDann <danilodemeloribeiro0@gmail.com>
2024-07-30 14:45:40 -0300

    Request model refrigerations

--- frontend/sige_ie/lib/equipments/data/refrigerations/refrigerations_request_model.dart
+++ frontend/sige_ie/lib/equipments/data/refrigerations/refrigerations_request_model.dart
@@ -1,16 +1,21 @@
 class RefrigerationsRequestModel {
   int? area;
   int? system;
+  int? quantity;
+  int? power;
 
-  RefrigerationsRequestModel({
-    required this.area,
-    required this.system,
-  });
+  RefrigerationsRequestModel(
+      {required this.area,
+      required this.system,
+      required this.quantity,
+      required this.power});
 
   Map<String, dynamic> toJson() {
     return {
       'area': area,
       'system': system,
+      'quantity': quantity,
+      'power': power
     };
   }
 }
