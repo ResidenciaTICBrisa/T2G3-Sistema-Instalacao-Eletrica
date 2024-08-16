6222ae6804c091a87982297e2f24d191b73cf2ca
OscarDeBrito <oscarcorreiadebrito@gmail.com>
2024-08-16 17:42:10 -0300

    Frontend: Termina tela de alterar username
    Co-authored-by: Pedro Lucas <garcia.lucas@aluno.unb.br>

--- frontend/sige_ie/lib/users/feature/change_username.dart
+++ frontend/sige_ie/lib/users/feature/change_username.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sige_ie/config/app_styles.dart';
+import 'package:sige_ie/users/data/user_response_model.dart';
+import 'package:sige_ie/users/data/user_service.dart';
+import 'package:sige_ie/users/data/username_request_model.dart';
 
 class ChangeUsernameScreen extends StatefulWidget {
   const ChangeUsernameScreen({super.key});
@@ -9,222 +12,178 @@ class ChangeUsernameScreen extends StatefulWidget {
 }
 
 class _ChangeUsernameScreenState extends State<ChangeUsernameScreen> {
-  final TextEditingController currentUsernameController =
-      TextEditingController();
-  final TextEditingController newUsernameController = TextEditingController();
-  final TextEditingController confirmUsernameController =
-      TextEditingController();
+  final UserService userService = UserService();
+  final TextEditingController usernameController = TextEditingController();
+
+  late UserResponseModel userResponseModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    try {
+      final fetchedUser = await userService.fetchProfileData();
+      setState(() {
+        userResponseModel = fetchedUser;
+        usernameController.text = userResponseModel.username;
+      });
+    } catch (error) {
+      // Tratamento de erro, pode-se exibir um dialog ou um Snackbar aqui
+      print('Erro ao carregar dados do usuário: $error');
+    }
+  }
+
+  Future<void> _changeUsername() async {
+    final newUsername = usernameController.text.trim();
+    if (newUsername.isEmpty) {
+      _showSnackBar('O nome de usuário não pode estar vazio.');
+      return;
+    }
+
+    try {
+      final usernameRequestModel = UsernameRequestModel(username: newUsername);
+      bool success = await userService.changeUsername(usernameRequestModel);
+      if (success) {
+        _showSnackBar('Usuário alterado com sucesso. Entre novamente.');
+        Navigator.pushReplacementNamed(context, '/loginScreen');
+      } else {
+        _showSnackBar('Falha ao alterar o username.');
+      }
+    } catch (error) {
+      // Tratamento de erro, pode-se exibir um dialog ou um Snackbar aqui
+      _showSnackBar('Erro ao alterar nome de usuário.');
+    }
+  }
+
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: AppColors.sigeIeBlue,
-        ),
-        body: SingleChildScrollView(
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Container(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-            decoration: const BoxDecoration(
-              color: AppColors.sigeIeBlue,
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
-            ),
-            child: const Center(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: AppColors.sigeIeBlue,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildHeader(),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'Mudar usuário',
-                    style: TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: _buildUsernameTextField(),
+                      ),
+                      const SizedBox(width: 10),
+                      _buildSaveButton(),
+                    ],
                   ),
+                  const SizedBox(height: 40),
+                  _buildBackButton(),
                 ],
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 20),
-                Text(
-                  'Usuário Atual',
-                  style: TextStyle(
-                    color: Colors.grey[600],
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 5),
-                TextField(
-                  controller: currentUsernameController,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 15.0, horizontal: 10.0),
-                    isDense: true,
-                  ),
-                ),
-                const SizedBox(height: 15),
-                Text(
-                  'Novo usuário',
-                  style: TextStyle(
-                    color: Colors.grey[600],
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 5),
-                TextField(
-                  controller: newUsernameController,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 15.0, horizontal: 10.0),
-                    isDense: true,
-                  ),
-                ),
-                const SizedBox(height: 15),
-                Text(
-                  'Confirmar Novo usuário',
-                  style: TextStyle(
-                    color: Colors.grey[600],
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 5),
-                TextField(
-                  controller: confirmUsernameController,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 15.0, horizontal: 10.0),
-                    isDense: true,
-                  ),
-                ),
-                const SizedBox(height: 40),
-                Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        width: 150,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            final currentUsername =
-                                currentUsernameController.text.trim();
-                            final newUsername =
-                                newUsernameController.text.trim();
-                            final confirmUsername =
-                                confirmUsernameController.text.trim();
+          ],
+        ),
+      ),
+    );
+  }
 
-                            if (currentUsername.isEmpty) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text(
-                                      'Por favor, insira seu usuário atual.'),
-                                ),
-                              );
-                            } else if (newUsername.isEmpty) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content:
-                                      Text('Por favor, insira o novo usuário.'),
-                                ),
-                              );
-                            } else if (confirmUsername.isEmpty) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text(
-                                      'Por favor, confirme o novo usuário.'),
-                                ),
-                              );
-                            } else if (newUsername == currentUsername) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text(
-                                      'O novo usuário não pode ser igual ao usuário atual.'),
-                                ),
-                              );
-                            } else if (newUsername != confirmUsername) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content:
-                                      Text('Os novos usuários não coincidem.'),
-                                ),
-                              );
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content:
-                                      Text('Usuário alterado com sucesso.'),
-                                ),
-                              );
-                              // Aqui você pode chamar o serviço de alteração do usuário
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: AppColors.sigeIeYellow,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            minimumSize: const Size(150, 50),
-                          ),
-                          child: const Text('Salvar',
-                              style: TextStyle(
-                                  color: AppColors.sigeIeBlue,
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w900)),
-                        ),
-                      ),
-                      const SizedBox(width: 20),
-                      SizedBox(
-                        width: 150,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                const Color.fromARGB(207, 231, 27, 27),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            minimumSize: const Size(150, 50),
-                          ),
-                          child: const Text('Voltar',
-                              style: TextStyle(
-                                  color: AppColors.lightText,
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w900)),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+  Widget _buildHeader() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+      decoration: const BoxDecoration(
+        color: AppColors.sigeIeBlue,
+        borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+      ),
+      child: const Center(
+        child: Text(
+          'Mudar Usuário',
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUsernameTextField() {
+    return TextField(
+      controller: usernameController,
+      enabled: true,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.grey[300],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide.none,
+        ),
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+        isDense: true,
+      ),
+    );
+  }
+
+  Widget _buildSaveButton() {
+    return ElevatedButton(
+      onPressed: _changeUsername,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.sigeIeYellow,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        minimumSize: const Size(150, 50),
+      ),
+      child: const Text(
+        'Salvar',
+        style: TextStyle(
+          color: AppColors.sigeIeBlue,
+          fontSize: 15,
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBackButton() {
+    return Center(
+      child: SizedBox(
+        width: 150,
+        height: 50,
+        child: ElevatedButton(
+          onPressed: () => Navigator.pop(context),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(207, 231, 27, 27),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            minimumSize: const Size(150, 50),
+          ),
+          child: const Text(
+            'Voltar',
+            style: TextStyle(
+              color: AppColors.lightText,
+              fontSize: 15,
+              fontWeight: FontWeight.w900,
             ),
           ),
-        ])));
+        ),
+      ),
+    );
   }
 }
